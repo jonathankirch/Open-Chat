6f149b6359c9ae31eba53f0066103bae97c31463
Jonathan Kirch <jonathankirch101@gmail.com>
2024-09-30 16:32:25 -0300

    add header of bypass tunnel on socket connection

--- src/app/SocketProvider.tsx
+++ src/app/SocketProvider.tsx
@@ -35,7 +35,14 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
       return
     }
 
-    const socket = io(`${backendUrl}`, { auth: { token } })
+    const socket = io(`${backendUrl}`, {
+      auth: { token },
+      transports: ['websocket'],
+      extraHeaders: {
+        'bypass-tunnel-reminder': '1'
+      }
+    });
+    
     socketRef.current = socket
 
     socket.on('connect', () => {
